921c7b2506effd000939cbe7aaf7d7dc48b20812
Liam Siira <liam@siira.io>
2023-06-21 19:37:57 -0400

    Added flag to disable email vs keybase

--- utils.py
+++ utils.py
@@ -30,54 +30,57 @@ def hasFlag(flg):
     return len(sys.argv) > 1 and flg in sys.argv[1]
 
 def send(template, subject, text, html = None):
-    logo = open(f"data/logo.png.htm").read()
-    msg = MIMEMultipart("alternative")
-
-    wrapper = open(f"templates/wrapper.html").read()
-    body = open(f"templates/{template}.html").read()
+    if not hasFlag("k"):
+        logo = open(f"data/logo.png.htm").read()
+        msg = MIMEMultipart("alternative")
     
-    wrapper = wrapper.replace("{subject}", subject)
-    wrapper = wrapper.replace("{host}", host)
-    wrapper = wrapper.replace("{logo}", logo)
-    wrapper = wrapper.replace("{body}", body)
-    wrapper = wrapper.replace("{desc}", desc)
-
-    if template == "code":
-        text = str(text)
-        wrapper = wrapper.replace("{text}", text)
-    elif template == "table":
-        text = str(text)
-        wrapper = wrapper.replace("{table}", html or text)
-    elif template == "update":
-        wrapper = wrapper.replace("{key1}", text["key1"])
-        wrapper = wrapper.replace("{val1}", text["val1"])
-        wrapper = wrapper.replace("{key2}", text["key2"])
-        wrapper = wrapper.replace("{val2}", text["val2"])
-    elif template == "speed":
-        wrapper = wrapper.replace("{ping}", str(text.ping))
-        wrapper = wrapper.replace("{upload}", str(text.upload))
-        wrapper = wrapper.replace("{download}", str(text.download))
-    else:
-        exit(1)
+        wrapper = open(f"templates/wrapper.html").read()
+        body = open(f"templates/{template}.html").read()
         
-    html = transform(wrapper)
-
-    # Record the MIME types of both parts - text/plain and text/html.
-    part1 = MIMEText(str(text), "plain")
-    part2 = MIMEText(html, "html")
-
-    # Attach parts into message container.
-    # According to RFC 2046, the last part of a multipart message, in this case
-    # the HTML message, is best and preferred.
-    msg.attach(part1)
-    msg.attach(part2)
-
-    msg["From"] = "Catalog879 <Catalog@pearwasps.io>"
-    msg["To"] = "Liam Siira <Liam@siira.io>"
-    msg["Subject"] = host + ": " + subject
-    p = Popen(["/usr/sbin/sendmail", "-t", "-oi"], stdin=PIPE)
-    p.communicate(msg.as_bytes())
-    os.system("run_keybase > /dev/null 2>&1")
-    os.system(f"keybase chat send pearwasps_io \"{host}: {subject}\" > /dev/null 2>&1")
+        wrapper = wrapper.replace("{subject}", subject)
+        wrapper = wrapper.replace("{host}", host)
+        wrapper = wrapper.replace("{logo}", logo)
+        wrapper = wrapper.replace("{body}", body)
+        wrapper = wrapper.replace("{desc}", desc)
+    
+        if template == "code":
+            text = str(text)
+            wrapper = wrapper.replace("{text}", text)
+        elif template == "table":
+            text = str(text)
+            wrapper = wrapper.replace("{table}", html or text)
+        elif template == "update":
+            wrapper = wrapper.replace("{key1}", text["key1"])
+            wrapper = wrapper.replace("{val1}", text["val1"])
+            wrapper = wrapper.replace("{key2}", text["key2"])
+            wrapper = wrapper.replace("{val2}", text["val2"])
+        elif template == "speed":
+            wrapper = wrapper.replace("{ping}", str(text.ping))
+            wrapper = wrapper.replace("{upload}", str(text.upload))
+            wrapper = wrapper.replace("{download}", str(text.download))
+        else:
+            exit(1)
+            
+        html = transform(wrapper)
+    
+        # Record the MIME types of both parts - text/plain and text/html.
+        part1 = MIMEText(str(text), "plain")
+        part2 = MIMEText(html, "html")
+    
+        # Attach parts into message container.
+        # According to RFC 2046, the last part of a multipart message, in this case
+        # the HTML message, is best and preferred.
+        msg.attach(part1)
+        msg.attach(part2)
+    
+        msg["From"] = "Catalog879 <Catalog@pearwasps.io>"
+        msg["To"] = "Liam Siira <Liam@siira.io>"
+        msg["Subject"] = host + ": " + subject
+        p = Popen(["/usr/sbin/sendmail", "-t", "-oi"], stdin=PIPE)
+        p.communicate(msg.as_bytes())
+    
+    if not hasFlag("m"):
+        os.system("run_keybase > /dev/null 2>&1")
+        os.system(f"keybase chat send pearwasps_io \"{host}: {subject}\" > /dev/null 2>&1")
 
 # send("TestingFunciton", "This is a test of an automated messaging script")
