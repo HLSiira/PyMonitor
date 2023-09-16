03d51feec704b01d0f0050584cbff8cddea3ab19
Liam Siira <liam@siira.io>
2023-09-16 19:54:58 -0400

    Migrated to pushover and cleaned up files/folders

--- examples/scripts/curtail.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#IFS= read var << EOF
-#$(foo)
-#EOF
-
-read -t 1 foo
-echo "You entered '$foo'"
--- utils.py
+++ utils.py
@@ -3,22 +3,20 @@
 # Use to inline: https://www.campaignmonitor.com/resources/tools/css-inliner/
 # Use to test emails: https://app.postdrop.io/
 # Use to check email: https://www.htmlemailcheck.com/check/
+
 import os, sys
 import socket
+import requests
+import json
 
 from subprocess import Popen, PIPE
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from datetime import datetime
 
-# sudo pip3 install premailer
-from premailer import transform
-
-
+# from premailer import transform
 host = socket.gethostname().title()
 name = os.path.basename(sys.argv[0])
-
-desc = "Automated emails are carefully planned emails to be sent to subscribers at specific time intervals or as a response to the actions of users on a particular website. These emails can be sent individually or as part of a drip email campaign."
 
 # SCANID is also the date/time
 SCANID = datetime.now().strftime("%Y%m%d%H%M")
@@ -28,60 +26,26 @@ def cPrint(msg):
 
 def hasFlag(flg):
     return len(sys.argv) > 1 and flg in sys.argv[1]
-
+    
+def load_credentials(filename="data/config.json"):
+    with open(filename, "r") as f:
+        data = json.load(f)
+    return data["user_key"], data["api_token"]
+    
 def send(template, subject, text, html = None):
-    if not hasFlag("k") and False:
-        logo = open(f"data/logo.png.htm").read()
-        msg = MIMEMultipart("alternative")
+    user_key, api_token = load_credentials()
     
-        wrapper = open(f"templates/wrapper.html").read()
-        body = open(f"templates/{template}.html").read()
-        
-        wrapper = wrapper.replace("{subject}", subject)
-        wrapper = wrapper.replace("{host}", host)
-        wrapper = wrapper.replace("{logo}", logo)
-        wrapper = wrapper.replace("{body}", body)
-        wrapper = wrapper.replace("{desc}", desc)
-    
-        if template == "code":
-            text = str(text)
-            wrapper = wrapper.replace("{text}", text)
-        elif template == "table":
-            text = str(text)
-            wrapper = wrapper.replace("{table}", html or text)
-        elif template == "update":
-            wrapper = wrapper.replace("{key1}", text["key1"])
-            wrapper = wrapper.replace("{val1}", text["val1"])
-            wrapper = wrapper.replace("{key2}", text["key2"])
-            wrapper = wrapper.replace("{val2}", text["val2"])
-        elif template == "speed":
-            wrapper = wrapper.replace("{ping}", str(text.ping))
-            wrapper = wrapper.replace("{upload}", str(text.upload))
-            wrapper = wrapper.replace("{download}", str(text.download))
-        else:
-            exit(1)
-            
-        html = transform(wrapper)
-    
-        # Record the MIME types of both parts - text/plain and text/html.
-        part1 = MIMEText(str(text), "plain")
-        part2 = MIMEText(html, "html")
-    
-        # Attach parts into message container.
-        # According to RFC 2046, the last part of a multipart message, in this case
-        # the HTML message, is best and preferred.
-        msg.attach(part1)
-        msg.attach(part2)
-    
-        msg["From"] = "Catalog879 <Catalog@pearwasps.io>"
-        msg["To"] = "Liam Siira <Liam@siira.io>"
-        msg["Subject"] = host + ": " + subject
-        
-        p = Popen(["/usr/sbin/sendmail", "-t", "-oi"], stdin=PIPE)
-        p.communicate(msg.as_bytes())
-    
-    if not hasFlag("m"):
-        os.system("run_keybase > /dev/null 2>&1")
-        os.system(f"keybase chat send pearwasps_io \"{host}: {subject}\" > /dev/null 2>&1")
+    data = {
+        'token': api_token,
+        'user': user_key,
+        'message': subject,
+        'title': host,
+        # 'monospace': 1,
+        'url': None,
+        'url_title': None,
+        'priority': 0,
+        'ttl': 43200
+    }
 
-# send("TestingFunciton", "This is a test of an automated messaging script")
+    response = requests.post('https://api.pushover.net/1/messages.json', data=data)
+    return response.text  # Returns the API's response which can be useful for debugging or confirmation
